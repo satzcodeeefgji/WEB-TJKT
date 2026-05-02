ee2474b35212482563913201b086ad944cb6ff29
satzcodeeefgji <satzcodeeefgji@users.noreply.github.com>
2026-05-02 15:18:25 +0700

    Deploy website to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <AuthProvider>
             <AppRoutes />
           </AuthProvider>
